ae53c8af9bb08cb3184f271f3b956f245b4323f2
gabriel aleixo <aleixo.gabriel.aleixo@gmail.com>
2024-11-13 21:40:43 -0300

    Registro e login

--- api/models/registroModels.js
+++ api/models/registroModels.js
@@ -5,10 +5,8 @@ module.exports = {
     registroPSW
 }
 
-function registroPSW(p_login, p_senha, p_email, p_sg, p_tel, p_end, p_cpf, callback){
-    // m_sql = "select * from usuarios where usu_apelido = '" + p_login + "' and  usu_password = '" + p_senha + "' ";
-    // m_sql = `INSERT INTO Usuario (nome_user, cpf_user, endereco_user, telefone_user, id_sangue, email_user, senha_user) VALUES ("${p_login}", "${p_cpf}", "${p_end}", "${p_tel}, "${p_sg}, "${p_email}, "${p_senha}")`;
-    m_sql = `INSERT INTO Usuario (nome_user, cpf_user, endereco_user, telefone_user, id_sangue, email_user, senha_user) VALUES ("${p_login}", "${p_cpf}", "${p_end}", "${p_tel}", "${p_sg}", "${p_email}", "${p_senha}")`;
+function registroPSW(p_login, p_senha, p_email, p_sg, p_tel, p_bairro, p_rua, p_num, p_cid, p_cpf, callback){
+    m_sql = `INSERT INTO Usuario (nome_user, cpf_user, bairro_user, rua_user, numero_user, cidade_user, telefone_user, id_sangue, email_user, senha_user) VALUES ("${p_login}", "${p_cpf}", "${p_bairro}", "${p_rua}", "${p_num}", "${p_cid}", "${p_tel}", "${p_sg}", "${p_email}", "${p_senha}")`;
 
 
 
