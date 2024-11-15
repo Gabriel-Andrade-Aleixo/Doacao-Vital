980cb885750fd9cbabded3ebc4dd940f76c14b4d
Uriel <141355502+AraujoUriel@users.noreply.github.com>
2024-11-15 20:06:59 -0300

    upando de leve.sql

--- doacaoVital.sql
+++ doacaoVital.sql
@@ -85,14 +85,20 @@ VALUES
 -- Inserindo dados na tabela Funcionarios
 INSERT INTO Funcionarios (nome_func, cpf_func, bairro_func, rua_func, numero_func, cidade_func, telefone_func, id_sangue, email_func, senha_func, cargo, data_nasc, id_hemocentro) 
 VALUES 
-('Carlos Oliveira', '45678912300', 'BairroC', 'Rua C', '789', 'CidadeC', '1199998888', 2, 'carlos@hemocentro.com', 'senha789', 'Enfermeiro', '1985-07-15', 1),
-('Ana Paula', '78912345600', 'BairroD', 'Rua D', '101', 'CidadeD', '1188889999', 4, 'ana@hemocentro.com', 'senha101', 'Recepcionista', '1990-03-20', 2);
+('ADMIN1', '123.456.789-00', 'BA', 'RA', '1234', 'CA', '(00) 12345-6789', 1, 'ADMON@admin.com', 'admin', 'ADMIN', '1212-12-12', 1);
 
 -- Inserindo dados na tabela Estoque
 INSERT INTO Estoque (tipo_sangue, volume_deposito) 
 VALUES 
-('A+', 10.5), 
-('O-', 8.0);
+('A+', 450.0), 
+('A-', 450.0), 
+('B+', 450.0), 
+('B-', 450.0), 
+('AB+', 450.0), 
+('AB-', 450.0), 
+('O+', 450.0), 
+('O-', 450.0);
+
 
 -- Consulta para verificar dados inseridos na tabela Usuario
 SELECT * FROM Usuario;
