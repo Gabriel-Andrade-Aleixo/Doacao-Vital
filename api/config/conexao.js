bd4e7a5e560a127d3dd80e2835580d220299b3cb
Gabriel-C137 <gabrielcamara250@gmail.com>
2024-11-18 02:36:03 -0300

    Navegação aprimorada e edição de localização

--- api/config/conexao.js
+++ api/config/conexao.js
@@ -3,7 +3,7 @@ var database = "doacaoVital";
 
 var conexao = mysql.createConnection({
     user: 'root',
-    password: '12345*',
+    password: '123456',
     host: 'localhost',
     port: 3306
 });
